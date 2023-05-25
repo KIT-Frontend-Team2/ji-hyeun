7cca6049563d341a870ee6046cb7cf224f9a6e90
jangdm37 <jangdm37@gmail.com>
2023-05-25 21:32:57 +0900

    chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    데이터 수정

---
+++ toy_project/src/data/post.js
@@ -0,0 +1,31 @@
+import { faker } from "@faker-js/faker";
+
+const createPostData = () => {
+  let imgBox = [];
+  for (let i = 1; i <= Math.floor(Math.random() * 5) + 1; i++) {
+    imgBox.push(faker.image.url());
+  }
+  return {
+    post_date: faker.date.between({ from: "2015-01-01", to: "2023-05-25" }),
+    user_name: faker.person.firstName(),
+    post_content: faker.lorem.lines(Math.floor(Math.random() * 10) + 6),
+    img_box: imgBox,
+  };
+};
+
+const GenerateData = (numUsers = 40) => {
+  return Array.from({ length: numUsers }, createPostData);
+};
+export const POST_DATA = GenerateData(48);
+
+// export const POST_DATA = [
+//   {
+//     post_date: faker.date.between("2015-01-01", "2023-05-25"),
+//     user_name: faker.person.fullName(),
+//     post_content: faker.lorem.lines({
+//       min: 5,
+//       max: Math.floor(Math.random()) * 10 + 6,
+//     }),
+//     img_box: imgBox,
+//   },
+// ];
