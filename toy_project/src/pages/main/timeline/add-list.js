3d5ad488c74920924d1fc44ba6797aecce0a4790
jangdm37 <jangdm37@gmail.com>
2023-05-30 22:30:24 +0900

    fix     : 유경님 코드 수정

--- toy_project/src/pages/main/timeline/add-list.js
+++ toy_project/src/pages/main/timeline/add-list.js
@@ -1,72 +1,64 @@
-
-import react from 'react'
-import styled  from "styled-components";
-import { useState } from 'react';
+import react from "react";
+import styled from "styled-components";
+import { useState } from "react";
+import { useTimeline } from "../../../context/timeline";
 // import { POST_DATA } from '../../../data/post';
 
+const AddTimeline = () => {
+  //새로운 객체를 생성
+  const [TimeLineList, dispatch] = useTimeline();
 
-const AddTimeline = ({timeline, setTimeline}) => {
+  const handleAdd = (e) => {
+    e.preventDefault();
 
-  //새로운 객체를 생성 
-  const [user_name, setTitle] = useState("")
-  const [post_content, setContent] = useState("")
+    dispatch({
+      type: "ADD",
+      payload: {
+        name: e.target.name.value,
+        content: e.target.content.value,
+      },
+    });
+  };
 
-
-  console.log(`타임라인 확인 `,timeline)
-
-
-  console.log(user_name, post_content)
-  const showMessage = (e) => {
-    e.preventDefault()
-    const newPost = {
-      postId : Math.random() * 100000,
-      user_name,
-      post_content,
-    }
-    console.log(newPost)
-    const updatePost = [...timeline, newPost]
-    setTimeline(updatePost)
-  } 
-
-  return(
+  return (
     <S.Wrapper>
-      <S.Form onSubmit={showMessage}> 
+      <S.Form onSubmit={(e) => handleAdd(e)}>
         <S.Title>
           <span>ADD TOTO LIST</span>
         </S.Title>
         <S.Content>
-          <input placeholder="제목을 입력해주세요" name="title" onChange={(e) =>setTitle(e.target.value)} />
-          <textarea placeholder="내용 입력" name="content" onChange={(e)=> setContent(e.target.value)} ></textarea>
+          <input placeholder="이름을 입력해주세요" name="name" />
+          <textarea placeholder="내용 입력" name="content"></textarea>
         </S.Content>
-        <S.Button >ADD</S.Button>
+        <S.Button>ADD</S.Button>
       </S.Form>
-  </S.Wrapper>
-  )
-}
+    </S.Wrapper>
+  );
+};
 
 export default AddTimeline;
 
 const Wrapper = styled.div`
-  border:  1px solid #333;
-`
+  border: 1px solid #333;
+`;
 
 const Form = styled.form`
-  border:  1px solid #333;
-`
+  border: 1px solid #333;
+`;
 
 const Title = styled.div`
-  border:  1px solid #333;
-`
+  border: 1px solid #333;
+`;
 const Content = styled.div`
-  border:  1px solid #333;
-`
+  border: 1px solid #333;
+`;
 const Button = styled.button`
-  border:  1px solid #333;
-`
+  border: 1px solid #333;
+`;
 const S = {
-  Wrapper, 
+  Wrapper,
   Form,
   Title,
   Content,
-  Button
-}
+  Button,
+};
