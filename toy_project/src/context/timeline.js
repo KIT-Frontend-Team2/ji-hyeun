05d6431d8d5dca4d9732219159a43a58d296ea04
DMJANG <96242198+hidongmin37@users.noreply.github.com>
2023-06-01 00:40:27 +0900

    Develop (#11)
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    기본 폴더구조 완성
    
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    페이지네이션 라이브러리 설치
    
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    페이커 js사용 부분 및 더미데이터구상
    
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    더미데이터 목록 생성 및 pagination페이지 구성
    
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    
    데이터 수정
    
    * chore   : 기타 변경사항 (빌드 업무 수정, 패키지 매니저 수정 등에 대한 커밋 등)
    
    데이터 수정
    
    * feat    : 페이지네이션 라이브러리없이 추가완료
    
    * feat    : 로그인 기능
    
    * crud1
    
    * chore   : git pull 전 충돌 전 수정
    
    * fix     : 유경님 코드 수정
    
    * fix     : 버그 (버그 수정) (#10)
    
    * fix     : 버그 (버그 수정)
    
    * fix     : 버그 (버그 수정)
    
    * readme/정리
    
    ---------
    
    Co-authored-by: Ji Hyeong Lee <iolpd42@gmail.com>
    Co-authored-by: BaeSangA <104805029+BaeSanga@users.noreply.github.com>
    Co-authored-by: Peter <djaudwls7873@gmail.com>
    Co-authored-by: uniend <dudnsgldns@naver.com>

---
+++ toy_project/src/context/timeline.js
@@ -0,0 +1,54 @@
+// 토이프로젝트 요구
+// 전역상태관리 : reducr+context
+import reducer from "../reducer/reducer";
+import { useState } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { POST_DATA } from "../data/post";
+//초기값생성
+const initialstate = POST_DATA;
+
+//저장소 생성
+
+const postReducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE":
+      return state.filter((item) => item.postId !== action.payload);
+    case "CHANGE":
+      // const newContent = e.target.value;
+
+      const newContent = action.payload.e.target.value;
+      if (action.payload.isEditMode) {
+        let revisePart = state.find((v) => v.postId === action.payload.id);
+        revisePart.post_content = newContent;
+      }
+      return state;
+    case "ADD":
+      const newPost = {
+        postId: Math.floor(Math.random() * 100000 + 1),
+        post_date: new Date(), // 현재 날짜와 시간을 설정합니다.
+        user_name: action.payload.name,
+        post_content: action.payload.content,
+        img_box: [],
+      };
+      return [...state, newPost];
+    default:
+      return state;
+  }
+};
+
+const TimeLineContext = createContext();
+
+export const useTimeline = () => useContext(TimeLineContext);
+
+//최상위 컴포넌트 생성
+// 하위 app.js에게 모두 전달하기 위해서
+
+export const TimeLineProvider = ({ children }) => {
+  const [TimeLineList, dispatch] = useReducer(postReducer, initialstate);
+
+  return (
+    <TimeLineContext.Provider value={[TimeLineList, dispatch]}>
+      {children}
+    </TimeLineContext.Provider>
+  );
+};
